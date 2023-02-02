f80ebc4343174c15f589e1f87fa5319a4018ab45
Jeffrey Scott <valenpendragon@gmail.com>
2023-02-02 11:31:30 -0500

    Set a more secure app password.

--- send_email.py
+++ send_email.py
@@ -1,11 +1,13 @@
 import smtplib, ssl
+import os
 
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "valenpendragon@gmail.com"
-    password = ""
+    # Get this password before testing.
+    password = os.getenv("PASSWORD")
     receiver = "valenpendragon@hotmail.com"
     context = ssl.create_default_context()
 
